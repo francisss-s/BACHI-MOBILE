ecda8cbb8164e0e19baab6ccfdb5ec0f5bac9248
Eduardo Hopperdietzel <eduardo@Eduardos-MacBook-Pro.local>
2018-12-11 00:26:05 -0300

    Left menu toggle

--- app/src/main/java/cl/uach/inf/bachimovil/EventsFragment.java
+++ app/src/main/java/cl/uach/inf/bachimovil/EventsFragment.java
@@ -1,14 +1,24 @@
 package cl.uach.inf.bachimovil;
 
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
+import android.graphics.Point;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.RequiresApi;
 import android.support.v4.app.Fragment;
 import android.util.Log;
+import android.view.Display;
+import android.view.DragEvent;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.HorizontalScrollView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import org.json.JSONArray;
@@ -17,11 +27,13 @@ import org.json.JSONObject;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class EventsFragment extends Fragment implements AsyncResponse{
-    String response;
-    String usr;
-    TextView mostrarConsulta;
-    Button btnConsultar;
+public class EventsFragment extends Fragment implements AsyncResponse
+{
+    // 0 - Anuncios 1 - Eventos
+    int state = 0;
+    boolean leftMenuOn = false;
+    LinearLayout leftMenu;
+    HorizontalScrollView hScroll;
     View view;
 
     public EventsFragment() {
@@ -34,19 +46,67 @@ public class EventsFragment extends Fragment implements AsyncResponse{
         // Inflate the layout for this fragment
 
         view = inflater.inflate(R.layout.fragment_eventos, container, false);
+        leftMenu = view.findViewById(R.id.LeftMenu);
+        hScroll = view.findViewById(R.id.HScroll);
 
-        mostrarConsulta = view.findViewById(R.id.resultadoConsulta);
-        btnConsultar = view.findViewById(R.id.btnConsulta);
+        hScroll.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                return true;
+            }
+        });
+
 
 
         return view;
 
     }
 
+    public void toggleLeftMenu(boolean state)
+    {
+
+        if(state)
+        {
+            animateMenu((int)((float)getResources().getDisplayMetrics().widthPixels * 0.8f), 128);
+        }
+        else
+        {
+            animateMenu(0, 128);
+        }
+        leftMenuOn = state;
+    }
+
+    public void animateMenu(int width, int duration)
+    {
+        ValueAnimator anim = ValueAnimator.ofInt(leftMenu.getMeasuredWidth(), width);
+        anim.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator valueAnimator) {
+                int val = (Integer) valueAnimator.getAnimatedValue();
+                ViewGroup.LayoutParams layoutParams = leftMenu.getLayoutParams();
+                layoutParams.width = val;
+                leftMenu.setLayoutParams(layoutParams);
+            }
+        });
+        anim.setDuration(duration);
+        anim.start();
+    }
 
 
     @RequiresApi(api = Build.VERSION_CODES.N)
     public void onClick(View v) {
+
+        switch (v.getId())
+        {
+            // Left Menu Button
+            case R.id.MenuButton:
+                {
+                    toggleLeftMenu(!leftMenuOn);
+                }
+                break;
+        }
+
+        /*
         if(v.getId() == R.id.btnConsulta) {
 
             int randomNum = (int) (24*Math.random()+1);
@@ -55,6 +115,7 @@ public class EventsFragment extends Fragment implements AsyncResponse{
             serviceManager.callService("http://146.83.216.206/info104/getMaterials.php?tags=");
 
         }
+        */
 
     }
 
@@ -69,7 +130,7 @@ public class EventsFragment extends Fragment implements AsyncResponse{
                 text += jsonArray.getJSONObject(i).getString("url")+"\n";
             }
 
-            mostrarConsulta.setText(text);
+            //mostrarConsulta.setText(text);
 
         }
         catch(Exception e)
